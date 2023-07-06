f2092d0c03c53cadf3185322bbd22f35786445c6
Titou82Bi <inacio.baptiste@gmail.com>
2023-07-06 09:06:36 +0200

    commit Concert

---
+++ src/app/concert/formulaire-concert/formulaire-concert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire-concert',
+  templateUrl: './formulaire-concert.component.html',
+  styleUrls: ['./formulaire-concert.component.css']
+})
+export class FormulaireConcertComponent {
+
+}
---
+++ src/app/concert/update-concert/update-concert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-concert',
+  templateUrl: './update-concert.component.html',
+  styleUrls: ['./update-concert.component.css']
+})
+export class UpdateConcertComponent {
+
+}
