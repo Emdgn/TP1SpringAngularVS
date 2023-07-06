dbdbf80f5988ae5bf7380a5e14bc06dcbf1f1d40
sarahborkan <sarah.behaz1@gmail.com>
2023-07-06 09:13:27 +0200

    modif

--- src/app/lieu/afficher-lieux/afficher-lieux.component.ts
+++ src/app/lieu/afficher-lieux/afficher-lieux.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Lieu } from 'src/app/models/lieu.model';
+import { LieuService } from 'src/app/services/lieu.service';
 
 @Component({
   selector: 'app-afficher-lieux',
   templateUrl: './afficher-lieux.component.html',
   styleUrls: ['./afficher-lieux.component.css']
 })
-export class AfficherLieuxComponent {
+export class AfficherLieuxComponent implements OnInit {
+  lieux: Lieu[] = [];
 
+  constructor(private lieuService: LieuService) { }
+
+  ngOnInit(): void {
+    this.getListeLieux();
+  }
+
+  getListeLieux() {
+    this.lieuService.getListeLieu().subscribe((lieux: Lieu[]) => {
+      this.lieux = lieux;
+    });
+  }
+
+  supprimerLieu(id: number) {
+    this.lieuService.deleteLieu(id).subscribe(() => {
+      this.getListeLieux();
+    });
+  }
 }
